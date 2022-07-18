9dab534d2a24b0cec8640a3ba12a265dfac9036f
Diego Saldano <dsaldano@preminens.com.ar>
2022-07-18 10:40:15 -0300

    feat: start project and add nav bar component

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import NavBar from './NavBar';
+import styles from './Layout.module.css';
+
+type LayoutProps = {
+    children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <div className="bg-gray-100">
+            <NavBar />
+            <main>
+              {children}
+            </main>
+        </div>
+    )
+}
+
+export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/global.css'
+import '../styles/global.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Layout from '../components/Layout'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+
+  return(
+   <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Mercado Libre - Envíos gratis en el día</title>
+        <meta name="description" content="Mercado Libre - Envíos gratis en el día" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+   </>
+  )
 }
+
+export default MyApp
